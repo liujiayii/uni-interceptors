13bfd0d069d6c09518cef0a8e14df144a2d21a4e
liujiayii <liujiayii@foxmail.com>
2025-09-28 14:19:16 +0800

    feat: 更新playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,8 @@
 import uni from "@dcloudio/vite-plugin-uni";
+import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [uni(), UnoCSS({})],
 });
