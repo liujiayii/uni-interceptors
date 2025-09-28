1fe2208956d56bdef6ceb26778002bec83788dd7
liujiayii <liujiayii@foxmail.com>
2025-09-28 16:10:14 +0800

    refactor(tools): 导出 AuthType 类型别名
    - 修改 authTips 模块的导出方式
    - 将 type AuthType 改为直接导出 AuthType- 保持其他工具函数导出不变

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,4 +1,4 @@
-export { authTips, type AuthType } from "./authTips";
+export { authTips, AuthType } from "./authTips";
 export { checkSelfPermission } from "./checkSelfPermission";
 export { shouldShowRequestPermissionRationale } from "./shouldShowRequestPermissionRationale";
 export { showAuthTip } from "./showAuthTip";
